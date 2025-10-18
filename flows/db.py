47ae3a9ba118c700fcac66de5f36bdacbac4119f
KlixMediaAds <ads.klix@gmail.com>
2025-10-18 12:16:37 -0400

    Update db.py

--- flows/db.py
+++ flows/db.py
@@ -1,11 +1,9 @@
+# klix/db.py
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL missing in environment")
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, future=True)
+engine = create_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
-Base = declarative_base()
